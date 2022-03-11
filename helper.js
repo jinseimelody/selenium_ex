63ab6a92cbfff884b6ec44ff86f1f62a1ba3ddfc
khoi.ngo <khoi.ngo@gss-sol.com>
2022-03-12 00:47:50 +0700

    update source

--- helper.js
+++ helper.js
@@ -1,32 +1,56 @@
+const config = require('./config.test.json');
 const fs = require('fs');
 const {
     v4: uuidv4
 } = require('uuid');
+const {
+    By,
+} = require('selenium-webdriver');
+
+const isNullOrEmpty = function (input) {
+    return input === undefined || input === null || input === '';
+}
+
+exports.ensureOutputDir = function () {
+    const directories = config.evidenceFolder.split("/").reverse();
+    let path = "";
+    while (directories.length > 0) {
+        const directory = directories.pop();
+        if (isNullOrEmpty(directory)) continue;
+
+        path += path === "" ? directory : "/" + directory;
+        if (!fs.existsSync(path)) {
+            console.log("create: " + path);
+            fs.mkdirSync(path);
+        }
+    }
+}
 
 exports.sleep = async function (ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+exports.login = async function (driver) {
+    await driver.get(config.baseUrl + "/login");
+    // login to github
+    const loginField = await driver.findElement(By.id("login_field"));
+    loginField.click();
+    loginField.sendKeys(config.userName);
+
+    const password = await driver.findElement(By.id("password"));
+    password.click();
+    password.sendKeys(config.password);
+
+    await driver.findElement(By.name("commit")).click();
+};
 
 // driver: selenium driver
-exports.takeScreenShot =  async function (folderName, driver) {
+exports.takeScreenShot = async function (driver) {
     const encodedString = await driver.takeScreenshot();
-    if (!fs.existsSync('output')) {
-        fs.mkdirSync('output');
-    }
-
-    if (!fs.existsSync('output/images')) {
-        fs.mkdirSync('output/images');
-    }
-
-    const path = "output/images/" + folderName;
-    if (!fs.existsSync(path)) {
-        fs.mkdirSync(path);
-    }
-
-    const relativePath = path + '/' + uuidv4() + '.png';
-    console.log(relativePath);
+    const fileName = uuidv4() + '.png';
+    const relativePath = config.evidenceFolder + "/" + fileName;
     await fs.writeFileSync(relativePath, encodedString, 'base64');
+    return fileName;
 }
 
 exports.findElement = async function (driver, locator, limit) {
@@ -50,7 +74,7 @@ exports.findElement = async function (driver, locator, limit) {
                 helper.log("waiting time limit exceeded", "red");
                 break;
             }
-        } catch { }
+        } catch {}
     }
 
     return element;
