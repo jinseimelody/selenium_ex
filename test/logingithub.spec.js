ac2b9ad439df30a40b6885ce1deacb4d9eb586c6
khoi.ngo <khoi.ngo@gss-sol.com>
2022-03-11 16:09:34 +0700

    update source

--- test/logingithub.spec.js
+++ test/logingithub.spec.js
@@ -8,10 +8,11 @@ const {
 const { takeScreenShot, findElement, sleep } = require('../helper');
 const config = require('../config/github_login_config.json');
 
-describe('login git hub', function () {
+describe(config.description.title, function () {
     this.timeout(30000);
     let driver;
     let vars;
+
     beforeEach(async function () {
         driver = await new Builder().forBrowser('chrome').build();
         vars = {};
@@ -19,7 +20,7 @@ describe('login git hub', function () {
     afterEach(async function () {
         await driver.quit();
     })
-    it('login git hub', async function () {
+    it('login and redirect to selenium_ex repository', async function () {
         await driver.get("https://github.com/");
         await driver.manage().window().setRect({
             width: 1936,
@@ -38,12 +39,40 @@ describe('login git hub', function () {
         await takeScreenShot(config.name, driver)
         await driver.findElement(By.name("commit")).click();
         const element = await findElement(driver, By.linkText("jinseimelody/selenium_ex"))
-        await driver.actions({async: true}).move({origin:element}).perform()
-        
+        await driver.actions({ async: true }).move({ origin: element }).perform()
+
         await sleep(300);
         await takeScreenShot(config.name, driver)
         await driver.findElement(By.linkText("jinseimelody/selenium_ex")).click()
 
+        await sleep(300);
+        await takeScreenShot(config.name, driver)
+    })
+
+    it('login and redirect to profile', async function () {
+        await driver.get("https://github.com/");
+        await driver.manage().window().setRect({
+            width: 1936,
+            height: 1056
+        })
+        await takeScreenShot(config.name, driver)
+        await driver.findElement(By.linkText("Sign in")).click()
+        const loginField = await driver.findElement(By.id("login_field"))
+        loginField.click()
+        loginField.sendKeys(config.input.userName)
+
+        const password = await driver.findElement(By.id("password"))
+        password.click()
+        password.sendKeys(config.input.password)
+
+        await takeScreenShot(config.name, driver)
+        await driver.findElement(By.name("commit")).click();
+        const avartar = await driver.findElement(By.css('.avatar.avatar-small.circle'))
+        avartar.click()
+
+        const profileLink = await findElement(driver, By.css("a[data-ga-click='Header, go to profile, text:your profile']"))
+        await driver.actions({ async: true }).move({ origin: profileLink }).perform()
+        profileLink.click();
         await sleep(300);
         await takeScreenShot(config.name, driver)
     })
